ae5136b6a5f8c259f329c5d5e30efe6049b5368e
mstnreddy <mstnreddy@gmail.com>
2018-06-28 12:29:15 +0530

    hello.java added

---
+++ Hello.java
@@ -0,0 +1,12 @@
+package helloJenkins;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world");
+		
+		System.out.println("Iam from Master branch");
+	}
+
+}
