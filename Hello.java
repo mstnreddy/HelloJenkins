79a573f4b547f73ed6fcf3f962d9e37866991c3d
mstnreddy <40627016+mstnreddy@users.noreply.github.com>
2018-06-28 14:29:48 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world");
 		
-		System.out.println("Iam from Master branch");
+		System.out.println("Iam from Rfe branch");
 	}
 
 }
