a443cd4bb988fef7b9b7feb398a0888634e5937a
mstnreddy <mstnreddy@gmail.com>
2018-06-29 14:28:29 +0530

    update sysout

--- Hello.java
+++ Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 		System.out.println("Hello, world");
 		
 		System.out.println("Iam from bug branch");
+		System.out.println("Triggered automatically ..............");
 	}
 
 }
